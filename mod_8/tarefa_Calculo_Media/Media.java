c3fce2446d0ebc89ffd77d7a6893b5c5ad17542c
Tote <weasd99843@hotmail.com>
2023-05-17 21:32:24 -0300

    Media
    Primeiro commit da classe Media.java

---
+++ mod_8/tarefa_Calculo_Media/Media.java
@@ -0,0 +1,13 @@
+public class Media {
+    public static void main(String[] args) {
+
+        int n1 = 10;
+        int n2 = 6;
+        int n3 = 4;
+        int n4 = 8;
+
+        int media = (n1 + n2 + n3 + n4) / 4;
+
+        System.out.println(media);
+    }
+}
