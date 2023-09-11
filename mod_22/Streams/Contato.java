1088f0c43c96b4931513146dd71403d4b65c893d
Tote <weasd99843@hotmail.com>
2023-09-11 19:27:10 -0300

    tarefa
    primeiro commit

---
+++ mod_22/Streams/Contato.java
@@ -0,0 +1,17 @@
+public class Contato {
+    private String nome;
+    private String genero;
+
+    public Contato(String nome, String genero) {
+        this.nome = nome;
+        this.genero = genero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+}
